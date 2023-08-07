e243257d846d93c30ae170f7af77a879683f1584
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-08-07 22:20:57 +0530

    added the home logic to display tweets.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { tweetType } from '../../../common/src';
 
 const Home: React.FC = () => {
+	const navigate = useNavigate();
+	const [tweets, setTweets] = useState([]);
+
+	useEffect(() => {
+		const token = window.localStorage.getItem('token');
+		async function getTweets() {
+			const response = await axios.get('http://localhost:3000/api/v1/tweet', {
+				headers: {
+					Authorization: token,
+				},
+			});
+			setTweets(response.data.tweets);
+		}
+
+		if (!token) {
+			navigate('/login');
+		} else {
+			getTweets();
+		}
+	}, []);
+
 	return (
 		<div>
-			<h1>Home...!</h1>
+			{tweets.map((tweet: tweetType) => {
+				return <h1 key={Math.random()}>{tweet.content}</h1>;
+			})}
 		</div>
 	);
 };
